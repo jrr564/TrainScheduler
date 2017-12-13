eb2c6c798f350ef46301774ed28113782b799f0c
admin <admin@vraney-mbp.thelamplighterschool.org>
2017-12-13 11:39:45 -0600

    final form

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,4 +1,4 @@
-//Firebase
+//Initialize Firebase
 var config = {
     apiKey: "AIzaSyDPOq9FNbYmW7KxGXBxu38p7gxuwGcUazk",
     authDomain: "trainscheduler-f5c2b.firebaseapp.com",
@@ -7,17 +7,24 @@ var config = {
     storageBucket: "",
     messagingSenderId: "504285094535"
   };
+
   firebase.initializeApp(config);
 
 var database = firebase.database();
+
+// var trainName = "";
+// var trainDestination = "";
+// var trainTime = 0;
+// var trainFreq = "";
+
 //button to add train
 $("#add-train-btn").on("click", function(event) {
 	event.preventDefault();
 //uses input
-	var trainName = $(".name-input").val().trim();
-	var trainDestination = $(".destination-input").val().trim();
-	var trainTime = $(".train-time").val().trim();
-	var trainFreq = $(".train-freq").val().trim();
+	var trainName = $("#name-input").val().trim();
+	var trainDestination = $("#destination-input").val().trim();
+	var trainTime = moment($("#time-input").val().trim(), "HH:mm").subtract(10, "years").format("X");
+	var trainFreq = $("#freq-input").val().trim();
 //Creates local object for holding train data
 	var newTrain = {
 		name: trainName,
@@ -26,12 +33,47 @@ $("#add-train-btn").on("click", function(event) {
 		frequency: trainFreq
 	};
 
-	database.ref().push(newEmp);
+	database.ref().push(newTrain);
 
 	console.log(newTrain.name);
 	console.log(newTrain.destination);
 	console.log(newTrain.time);
 	console.log(newTrain.frequency);
 
-
+	$("#name-input").val("");
+  	$("#destination-input").val("");
+  	$("#time-input").val("");
+  	$("#freq-input").val("");
 });
+
+//creates Firebase event + adds row
+database.ref().on("child_added", function(childSnapshot, prevChildkey) {
+
+	var trainName = childSnapshot.val().name;
+	var trainDestination = childSnapshot.val().destination;
+	var trainTime = childSnapshot.val().time;
+	var trainFreq = childSnapshot.val().frequency;
+
+//calculate next arrival
+
+	var differenceTimes = moment().diff(moment.unix(trainTime), "minutes");
+	var remainder = moment().diff(moment.unix(trainTime), "minutes") % trainFreq ;
+	var minutes = trainFreq - remainder;
+	console.log(minutes)
+
+	var arrival = moment().add(minutes, "m").format("hh:mm A"); 
+
+
+	$("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+  	trainFreq + "</td><td>" + arrival + "</td><td>" + minutes + "</td><td>");
+});
+
+
+
+
+
+
+
+
+
+
